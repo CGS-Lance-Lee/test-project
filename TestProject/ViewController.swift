687621671980042d3cc01d3732a94c87c1d2237a
Lance Lee <38645@cgs.act.edu.au>
2015-07-24 12:51:36 +1000

    Initial commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Lance Lee on 24/07/2015.
+//  Copyright (c) 2015 Canberra Grammar School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        println("Screw my life")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
